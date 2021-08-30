e8e7dbc00618a95be0b3e5e4f59c64ae25eeba55
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-08-30 09:02:32 +0600

    up

--- js/app.js
+++ js/app.js
@@ -4,14 +4,22 @@ function getInputValue(id) {
   return val;
 
 }
+function showError(errorMessage) {
+    document.getElementById('error-message').innerText = errorMessage;
+}
 function fetchData(url) {
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayData(data.meals));
+    .then((data) => displayData(data.meals))
+    .catch(error => {
+        setVisible('error-message');
+        showError('Network Error!');
+      });
 }
 function displayData(allData) {
     if(allData == null) {
-        setVisible('result-message');
+        showError('No Result Found!');
+        setVisible('error-message');
     }
     else {
         for(data of allData) {
@@ -20,9 +28,10 @@ function displayData(allData) {
             col.innerHTML = `
             <div class="card h-100">
                 <img src="${data.strMealThumb}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">${data.strMeal}</h5>
-                <p class="card-text">${data.strInstructions.slice(0,100)}</p>
+                <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                    <h2 class="card-title">${data.strMeal}</h2>
+                    <p class="card-text">${data.strInstructions.slice(0,100)}</p>
+                    <a href="">Buy Now <i class="fas fa-long-arrow-alt-right"></i></a>
                 </div>
             </div>
             `
@@ -37,13 +46,13 @@ function setInvisible(id) {
     document.getElementById(id).style.display = 'none';
 }
 document.getElementById("button-search").addEventListener("click", function (event) {
+    setVisible('spinner');
+    setInvisible('error-message');
     setTimeout(() => {
         setInvisible('spinner');
     }, (200));
     const searchText = getInputValue("input-meal");
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     document.getElementById('row').textContent = '';
-    setVisible('spinner');
-    setInvisible('result-message');
     fetchData(url);
 });
